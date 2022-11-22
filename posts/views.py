b3c6ea87600fbe2511df950ee070f0c45498ffb1
Mumtaz Eralp Unver <eralpunver85@gmail.com>
2022-11-22 11:12:30 +0300

    Added comments to posts

--- posts/views.py
+++ posts/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
-from .forms import PostForm
+from .forms import PostForm,CommentForm
 from django.shortcuts import redirect
 
 # Create your views here.
@@ -52,3 +52,16 @@ def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'posts/add_comment_to_post.html', {'form': form})
