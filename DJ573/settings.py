f6bf7d1197a00d4e74127e320dbee0ba9befc6df
Mumtaz Eralp Unver <eralpunver85@gmail.com>
2022-12-06 17:56:09 +0300

    Setting change

--- DJ573/settings.py
+++ DJ573/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-0p=i90sg-d9_3(ccve2p*p-d(h*)eugun)te%-)m8qb=&(ekw9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0", "*"]
 
 
 # Application definition
