51abaafd0c9332cdc7bd60863557946a362784ab
Mumtaz Eralp Unver <eralpunver85@gmail.com>
2022-12-11 20:50:18 +0300

    New tests are added to test search function. #10
    Docker compose file has been debugged. #14

--- posts/tests.py
+++ posts/tests.py
@@ -4,6 +4,7 @@ from .models import Post
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate, logout
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 
 # Create your tests here.
 
@@ -12,7 +13,7 @@ class PostTestCase(TestCase):
     def setUp(self):
         User.objects.create_user(username ='eralp',email="abc@abc.com", password="1234")
         me = User.objects.get(username="eralp")
-        Post.objects.create(author= me, title ="Test the Test", text= "Testing the Test Works")
+        Post.objects.create(author= me, title ="Test the Test", text= "Testing the Test Works",upload='a')
 
     def test_post_exists(self):
         p = Post.objects.all()
@@ -60,3 +61,31 @@ class PostTestCase(TestCase):
         user = authenticate(username='sevval', password='1234')
         self.assertFalse(user)
 
+    def test_search(self):
+        me = User.objects.get(username="eralp")
+        # Create a few test posts
+        post1 = Post.objects.create(author=me,title="Test Post 1", text="This is the first test post",upload='x')
+        post2 = Post.objects.create(author=me,title="Test Post 2", text="This is the second test post",upload='y')
+
+        # Test searching for a query that matches the title of one of the posts
+        searched = "Test Post 1"
+        posts_s_ = Post.objects.filter(Q(title__icontains=searched) | Q(text__icontains=searched))
+        self.assertTrue(posts_s_.exists())
+        self.assertEqual(posts_s_.count(), 1)
+        self.assertEqual(posts_s_.first(), post1)
+
+        searched = "Test Post 2"
+        posts_s_ = Post.objects.filter(Q(title__icontains=searched) | Q(text__icontains=searched))
+        self.assertTrue(posts_s_.exists())
+        self.assertEqual(posts_s_.count(), 1)
+        self.assertEqual(posts_s_.first(), post2)
+
+        searched = "test"
+        posts = Post.objects.filter(Q(title__icontains=searched) | Q(text__icontains=searched))
+        self.assertTrue(posts.exists())
+        self.assertEqual(posts.count(), 3)
+
+        searched = "eralp"
+        posts = Post.objects.filter(Q(title__icontains=searched) | Q(text__icontains=searched))
+        self.assertFalse(posts.exists())
+        self.assertEqual(posts.count(), 0)
