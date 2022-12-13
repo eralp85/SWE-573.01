8beafff4744ecb794a42a91754d1f511fb2539f6
Mumtaz Eralp Unver <eralpunver85@gmail.com>
2022-12-13 19:54:37 +0300

    The authors and tags are created in models.py #15

--- posts/models.py
+++ posts/models.py
@@ -36,3 +36,19 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.text
+
+class Author(models.Model):
+    user = models.OneToOneField
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.text
