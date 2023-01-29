9ba01b57740f7baba94c65cfce2e75b93390f394
Mumtaz Eralp Unver <eralpunver85@gmail.com>
2023-01-29 17:57:40 +0300

    Follow function bug fix

--- posts/views.py
+++ posts/views.py
@@ -259,7 +259,7 @@ def user_follow(request):
                 Contact.objects.get_or_create(
                     user_from=request.user,
                     user_to=user)
-                create_action(request.user, 'is following', user)
+
             else:
                 Contact.objects.filter(user_from=request.user,
                                        user_to=user).delete()
