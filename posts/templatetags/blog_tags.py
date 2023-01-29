71676b2bb84f22b27a14e4dbda1ee7333f5141c7
Mumtaz Eralp Unver <eralpunver85@gmail.com>
2023-01-29 16:45:06 +0300

    Bug fix on follow requests.

--- posts/templatetags/blog_tags.py
+++ posts/templatetags/blog_tags.py
@@ -1,9 +1,20 @@
 from django import template
 from ..models import Post
+from django.db.models import Count
+
 
 register = template.Library()
 
 
 @register.simple_tag
 def total_posts():
-    return Post.published.count()
+
+    return Post.objects.filter()
+
+
+@register.inclusion_tag('posts/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.objects.order_by('-published_date')[:count]
+    return {'latest_posts': latest_posts}
+
+
